645f9c4fb573d7368b5e9959a78b241716bcef28
wb-hf551956 <wb-hf551956@alibaba-inc.com>
2022-06-23 16:04:18 +0800

    测试

--- demo/day1/去重脚本.py
+++ demo/day1/去重脚本.py
@@ -1,40 +1,14 @@
 # Author:hufei
 # -*- coding: utf-8 -*-
-import re
-#
-# list01 =[];
-# list02 =[];
-# file = '/demo/day1/long.txt'
-# f = open(r"long.txt", "rb")
-# str =f.readlines()
-# with
-#
-# for line in str :
-#     print(str)
-#     out_file=set()
-#     list02 = line.split("locationCode",2)
-#     print(list02)
-#     if line in list01:
-#         continue
-#     list01.append(line)
-#     list02 = list01.split()
-#     f.close()
-# print(list02)
-#
-
-file_list =[] #创建空列表
 def out_file():
-    file = "long.txt" #需要去重的文件
-    with open(file,'rb') as f:
-        file_2 = f.read().decode().split('},')
-        for file in file_2:
-            file_list.append(file)
-        out_file1 =set(file_list)#set()函数可以自动过滤掉重复元素
-        last_out_file = list(out_file1)
-        for out in last_out_file:
-            with open("result.txt", "a+") as f:  # 去重后文件写入文件里
-                f.write(out + "\n")
-                print(out)
-
+    oldfile = "long.txt"  # 需要去重的文件
+    with open(oldfile, 'rb') as f:
+        with open("result.txt", "a+",encoding="utf-8-sig") as g:
+            for line in f.read().decode().split('},'): #分行
+                    if key_word not in line:
+                        g.write(line + "\n")
+                        print(line)
 if __name__ =="__main__":
+    key_word = input("清输入你想去掉的关键字：")
     out_file()
+
