74656a7dd0946ba8ef1e10f994a8ef7669e21781
wb-hf551956 <wb-hf551956@alibaba-inc.com>
2022-06-28 17:53:22 +0800

    测试

--- demo/day3/list.py
+++ demo/day3/list.py
@@ -23,5 +23,5 @@ print("demo2代码运行时间：",m_time-f_time)
 #返回最大值, lambda 匿名函数
 max = lambda a,b : a if a>b else b
 r = max(10,15)
-if r != 10 :   #断言，如果r不等于10就报错
+if r != 15 :   #断言，如果r不等于10就报错
     raise AssertionError
